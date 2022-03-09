688d64e1fa61c77e36427849e47bf5133e82bbe6
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-03-09 12:55:56 -0300

    feat: project structure and docker connection

---
+++ src/server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const fileStore = require("session-file-store")(session);
+const flash = require("express-flash");
+
+const app = express();
+
+require("./database");
+
+// Template engine
+app.engine(
+  "handlebars",
+  exphbs.engine({ extname: ".hbs", defaultLayout: "main" })
+);
+app.set("view engine", "handlebars");
+
+// Receber respostas do body
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+// Session middleware
+app.use(
+  session({
+    name: "session",
+    secret: "cT8pk904ZjwbrMzb7Z0Fm1ol4xVOHFj3",
+    resave: false,
+    saveUninitialized: false,
+    store: new fileStore({
+      logFn: function () {},
+      path: require("path").join(require("os").tmpdir(), "sessions"),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      express: new Date(Date.now() + 360000),
+      httpOnly: true,
+    },
+  })
+);
+
+// Flash messages
+app.use(flash());
+
+// Public path
+app.use(express.static("public"));
+
+// Set session to res
+app.use((req, res, next) => {
+  if (req.session.userid) {
+    res.locals.session = req.session;
+  }
+
+  next();
+});
+
+app.listen(3333);
