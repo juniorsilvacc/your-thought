90c52dc64763ed46c1c4bc15766f93a184172055
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-03-10 02:05:51 -0300

    chore: creation of migrations

--- src/database/migrations/20220309161840-create-toughts.js
+++ src/database/migrations/20220309161840-create-toughts.js
@@ -2,29 +2,29 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    return queryInterface.createTable("users", {
+    return queryInterface.createTable("toughts", {
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
         autoIncrement: true,
         allowNull: false,
       },
-      name: {
+      title: {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      email: {
-        type: Sequelize.STRING,
+      created_at: {
+        type: Sequelize.DATE,
         allowNull: false,
       },
-      password: {
-        type: Sequelize.STRING,
+      updated_at: {
+        type: Sequelize.DATE,
         allowNull: false,
       },
     });
   },
 
   async down(queryInterface, Sequelize) {
-    return queryInterface.dropTable("users");
+    return queryInterface.dropTable("toughts");
   },
 };
