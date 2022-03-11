f47bf0be26279a166b1906034eca41cff05c79e4
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-03-11 13:40:08 -0300

    feat: creating user registry

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,3 +1,6 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
 module.exports = {
   login(req, res) {
     res.render("auth/login");
@@ -5,5 +8,49 @@ module.exports = {
 
   register(req, res) {
     res.render("auth/register");
+  },
+
+  async registerPost(req, res) {
+    const { name, email, password, confirmePassword } = req.body;
+
+    if (password != confirmePassword) {
+      req.flash("message", "Senhas diferentes, tente novamente.");
+      res.render("auth/register");
+      return;
+    }
+
+    if (password > 6) {
+      req.flash("message", "A senha precisa ter no mínimo 6 caractere.");
+      res.render("auth/register");
+      return;
+    }
+
+    const checkIfUserExists = await User.findOne({ email });
+
+    if (checkIfUserExists) {
+      req.flash("message", "E-mail já cadastrado, tente novamente.");
+      res.render("auth/register");
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+
+    const PasswordHash = bcrypt.hashSync(password, salt);
+
+    const user = {
+      name,
+      email,
+      password: PasswordHash,
+    };
+
+    try {
+      await User.create(user);
+      req.flash("message-success", "Cadastro realizado com sucesso.");
+      res.render("auth/login");
+    } catch (err) {
+      console.log(err);
+    }
+
+    return;
   },
 };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,5 +5,6 @@ const AuthController = require("../controllers/AuthController");
 
 routes.get("/login", AuthController.login);
 routes.get("/register", AuthController.register);
+routes.post("/register", AuthController.registerPost);
 
 module.exports = routes;
