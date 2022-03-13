a6fd2579e17207f60e335aca14356097b3bc9a97
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-03-13 17:28:03 -0300

    feat: page dashboard

--- src/controllers/ToughtController.js
+++ src/controllers/ToughtController.js
@@ -2,6 +2,14 @@ const Tought = require("../models/Tought");
 
 module.exports = {
   async showToughts(req, res) {
-    res.render("home");
+    res.render("toughts/home");
+  },
+
+  async dashboard(req, res) {
+    res.render("toughts/dashboard");
+  },
+
+  createToughts(req, res) {
+    res.render("toughts/create");
   },
 };
