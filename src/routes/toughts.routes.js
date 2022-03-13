63a84c344e273ab0a415d40a3d0ccb2a63d7e48a
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-03-13 17:26:43 -0300

    feat: middleware checkAuth

--- src/routes/toughts.routes.js
+++ src/routes/toughts.routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const routes = express.Router();
 
+const checkAuth = require("../helpers/auth").checkAuth;
+
 const ToughtController = require("../controllers/ToughtController");
 
+routes.get("/dashboard", checkAuth, ToughtController.dashboard);
+routes.get("/add", ToughtController.createToughts);
 routes.get("/", ToughtController.showToughts);
 
 module.exports = routes;
